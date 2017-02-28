a541e5f4f1eb1bbb0c054290a48e782e36b69f1c
Veena Kumar <veenakumar@Veenas-MacBook-Pro.local>
2017-02-27 22:13:08 -0800

    pastgame

---
+++ PastGame.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image
+} from 'react-native'
+
+export default class LiveGame extends Component {
+  constructor(){
+    super()
+    this.state = {
+        sport: "Basketball",
+        team1: "UCSD",
+        score1: 21,
+        team2: "USC",
+        score2: 0
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Text style={styles.sport}> --{this.state.sport}-- </Text>
+        <View style={styles.container}>
+          <View style={styles.logo_item}>
+            <Image source={require('./home_bar_icons/home.png')} style={styles.logo}/>
+            <Text>{"\t\t"}</Text>
+          </View>
+          <View>
+            <Text style={styles.item}><Image source={require('./home_bar_icons/home.png')} style={styles.sport_image}/> {this.state.team1}  {this.state.score1}{"\t"} {this.state.score2}  {this.state.team2}  <Image source={require('./home_bar_icons/home.png')} style={styles.sport_image}/></Text>
+          </View>
+          <View>
+            <Text style={styles.time}>{"\n"}       00:00</Text>
+            <Text style={styles.time}>       1st</Text>
+            <Text style={styles.time}>{"\t"}(G.M?)</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    borderColor: 'grey',
+    borderWidth: 4
+  },
+  logo_item:{
+    alignItems: 'center'
+  },
+  item:{
+    fontFamily:"Menlo",
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 62,
+    textAlign:'center'
+  },
+  sport:{
+    fontFamily: 'Didot',
+    backgroundColor: 'gold',
+    fontWeight: 'bold',
+    borderColor: 'navy',
+    padding: 10,
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  sport_image: {
+    width: 25,
+    height: 50,
+    alignItems: 'center',
+    marginTop: 15
+  },
+  time: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  logo: {
+    width: 35,
+    height: 35,
+    marginTop: 10,
+    marginRight:15,
+    marginLeft: 15,
+    justifyContent: 'center'
+  }
+});
+
+AppRegistry.registerComponent('LiveGame', () => LiveGame);
