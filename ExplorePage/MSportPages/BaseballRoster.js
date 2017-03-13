ad756f97616d7fdcd02602baed48eb89a47fe0ee
Taylor Lew <taylorlew@taylors-mbp-2.dynamic.ucsd.edu>
2017-03-12 22:44:54 -0700

    figured out non divisible by 3

--- ExplorePage/MSportPages/BaseballRoster.js
+++ ExplorePage/MSportPages/BaseballRoster.js
@@ -11,13 +11,6 @@ import {
   TabBarIOS
 } from 'react-native';
 
-import Header from '../Header';
-import SportIcon from '../SportIcon';
-import NavBar from '../NavBar';
-import MenSports from '../MenSports';
-import WomenSports from '../WomenSports';
-import Slider from '../Slider.js';
-import BaseballPage from './BaseballPage.js';
 import RosterIcon from '../RosterIcon.js';
 
 const Dimensions = require('Dimensions');
@@ -25,36 +18,144 @@ const window = Dimensions.get('window');
 
 export default class BaseballRoster extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      size: 0,
+      names: [],
+      bioPage: []
+    }
+
+    { this.getRosterSize() }
+  }
+
+  getRosterSize() {
+     fetch('https://goatbackend110.appspot.com/static/rosters.json')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            size: Object.keys(responseJson.rosters["0"]).length
+          })
+
+          for (var i = 0; i < this.state.size; i++) {
+            this.setState({
+              names: this.state.names.concat([
+                responseJson.rosters["0"][i][0]
+              ]),
+
+              bioPage: this.state.bioPage.concat([
+                responseJson.rosters["0"][i][6]
+              ])
+            })
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+
+
   render() {
+    var roster = []
+
+    for (let i = 0; i < (this.state.size - (this.state.size % 3)); i += 3) {
+      var url1 = "https://goatbackend110.appspot.com/static/rosters/0/" + i + ".png"
+      var url2 = "https://goatbackend110.appspot.com/static/rosters/0/" + (i+1) + ".png"
+      var url3 = "https://goatbackend110.appspot.com/static/rosters/0/" + (i+2) + ".png"
+
+      roster.push(
+        <View style = {styles.roster_row}>
+          <TouchableHighlight >
+            <View style = {styles.iconLeft}>
+              <RosterIcon pic = {url1} name = {this.state.names[i]} />
+            </View>
+          </TouchableHighlight>
+
+          <TouchableHighlight>
+            <View style = {styles.icon}>
+              <RosterIcon pic = {url2} name = {this.state.names[i+1]} />
+            </View>
+          </TouchableHighlight>
+
+          <TouchableHighlight>
+            <View style = {styles.iconRight}>
+              <RosterIcon pic = {url3} name = {this.state.names[i+2]} />
+            </View>
+          </TouchableHighlight>
+        </View>
+      );
+    }
+
+    if (this.state.size % 3 != 0) {
+      if (this.state.size % 3 == 1) {
+        var url1 = "https://goatbackend110.appspot.com/static/rosters/0/" + (this.state.size - 1) + ".png"
+        roster.push(
+          <View style = {styles.roster_row}>
+            <TouchableHighlight>
+              <View style = {styles.iconLeft}>
+                <RosterIcon pic = {url1} name = {this.state.names[(this.state.size - 1)]} />
+              </View>
+            </TouchableHighlight>
+          </View>
+        );
+      }
+
+      else {
+        var url1 = "https://goatbackend110.appspot.com/static/rosters/0/" + (this.state.size - 2) + ".png"
+        var url2 = "https://goatbackend110.appspot.com/static/rosters/0/" + (this.state.size - 1) + ".png"
+
+        roster.push(
+          <View style = {styles.roster_row}>
+            <TouchableHighlight>
+              <View style = {styles.iconLeft}>
+                <RosterIcon pic = {url1} name = {this.state.names[(this.state.size - 2)]} />
+              </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight>
+              <View style = {styles.icon}>
+                <RosterIcon pic = {url2} name = {this.state.names[(this.state.size - 1)]} />
+              </View>
+            </TouchableHighlight>
+          </View>
+        );
+      }
+    }
+
     return (
-
-        <View style = {{flex: 1, marginTop: 10}}>
-
-
-        <View style = {{flex:1, flexDirection: 'row'}}>
-          <TouchableHighlight>
-            <View>
-              <RosterIcon pic = "https://goatbackend110.appspot.com/static/rosters/0/0.png"/>
-            </View>
-          </TouchableHighlight>
-
-          <TouchableHighlight>
-            <View>
-              <RosterIcon pic = "https://goatbackend110.appspot.com/static/rosters/0/1.png"/>
-            </View>
-          </TouchableHighlight>
-
-          <TouchableHighlight>
-            <View>
-              <RosterIcon pic = "https://goatbackend110.appspot.com/static/rosters/0/2.png"/>
-            </View>
-          </TouchableHighlight>
-        </View>
-
-        </View>
+      <View style = {styles.overall_page}>
+        <ScrollView>
+          { roster }
+        </ScrollView>
+      </View>
 
     );
   }
-};
+  };
+
+  const styles = StyleSheet.create({
+  overall_page: {
+    flex: 1,
+    marginTop: 10,
+    paddingBottom: 50
+  },
+
+  roster_row: {
+    flexDirection: 'row',
+  },
+
+  icon: {
+    paddingBottom: 3,
+  },
+
+  iconLeft: {
+    paddingRight: 3
+  },
+
+  iconRight: {
+    paddingLeft: 3
+  }
+
+  });
 
 AppRegistry.registerComponent('BaseballRoster', () => BaseballRoster);
